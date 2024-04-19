e27c379f2bad56701179b3421490cf6b26e5ca26
Gazi Akter <gaziakterdev@gmail.com>
2024-04-19 23:29:57 +0600

    plugin header

---
+++ wp-crud.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name:       WP Crud
+ * Plugin URI:        https://gaziakter.com/plugins/wp-crud/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Gazi Akter
+ * Author URI:        https://gaziakter.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-crud
+ * Domain Path:       /languages
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
